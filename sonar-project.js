2bb816cedc510e932d4ede2090c214f647472d02
BagepalliAravindreddy <78132257+BagepalliAravindreddy@users.noreply.github.com>
2021-02-16 17:15:05 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.169.156.101:9000/',
+  serverUrl: 'http://65.1.147.69:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
